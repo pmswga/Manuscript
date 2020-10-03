a6f875e92dff4fb0902a9dde1440de65070dedb4
Sergey <pmswga@gmail.com>
2020-10-03 21:22:20 +0400

    Create routes and main page

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,9 @@
+
+<div class="ui menu">
+    <a class="item" href="">Главная</a>
+    <a class="item" href="">Категории</a>
+    <div class="right menu">
+        <a class="item">О нас</a>
+        <a class="item">Контакты</a>
+    </div>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,42 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/searches', function () {
+
+});
+
+Route::get('/categories', function () {
+    return 'Categories';
+});
+
+Route::group(['prefix' => '/books'], function () {
+
+    Route::get('/', function () {
+
+    });
+
+    Route::get('/antiquarian', function () {
+        return 'Antiquarian books';
+    });
+
+    Route::get('/second-hand', function () {
+        return 'Second-hand books';
+    });
+
+    Route::get('/modern', function () {
+        return 'Modern books';
+    });
+
+});
+
+Route::get('/contacts', function () {
+    return 'This is contacts page';
+});
+
+Route::get('/about', function () {
+    return 'This is about page';
 });
